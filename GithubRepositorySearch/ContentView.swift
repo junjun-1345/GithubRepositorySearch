80bce62e4250eaf742ece4aa7c0f6d935fd0d2ad
junjun-1345 <junjun.sekaowa.1345@ezweb.ne.jp>
2024-05-17 14:02:31 +0900

    Initial Commit

---
+++ GithubRepositorySearch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GithubRepositorySearch
+//
+//  Created by 大野純平 on 2024/05/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
