25107b5d5245a1db39bb12bd15bdd39c9b161389
junjun-1345 <junjun.sekaowa.1345@ezweb.ne.jp>
2024-05-17 14:24:46 +0900

    Create view section

--- GithubRepositorySearch/ContentView.swift
+++ GithubRepositorySearch/ContentView.swift
@@ -1,24 +1,49 @@
-//
-//  ContentView.swift
-//  GithubRepositorySearch
-//
-//  Created by 大野純平 on 2024/05/17.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    // ViewModelをインスタンス化
+    //@ObservedObjectの属性が付いたオブジェクトのプロパティが変化すると、ビューは自動的に再描画されます。
+    @ObservedObject var viewModel = RepositoryViewModel()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                // $viewModel.queryの値とバインディング
+                TextField("Search Repositories", text: $viewModel.query)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+                
+                if viewModel.isLoading {
+                    VStack {
+                        ProgressView()
+                        Spacer()
+                    }
+                } else if let error = viewModel.error {
+                    Text("Error: \(error.localizedDescription)")
+                    Spacer()
+                } else {
+                    List(viewModel.repositories) { repository in
+                        VStack(alignment: .leading) {
+                            Text(repository.name)
+                                .font(.headline)
+                            Text(repository.description ?? "")
+                                .font(.subheadline)
+                            Link("View on GitHub", destination: URL(string: repository.html_url)!)
+                                .font(.caption)
+                                .foregroundColor(.blue)
+                        }
+                        .padding()
+                    }
+                    .listStyle(PlainListStyle())
+                }
+            }
+            .navigationTitle("GitHub Search")
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
